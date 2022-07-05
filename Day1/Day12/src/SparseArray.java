b8d6c958ebae126b797658db3050592b588ac7a9
Wenxin <1289261630@qq.com>
2022-07-05 10:42:05 +0800

    20220705

---
+++ Day1/Day12/src/SparseArray.java
@@ -0,0 +1,15 @@
+/**
+ * Create with IntelliJ IDEA
+ * Description:
+ * Author: Susie-Wen
+ * Date:2022-07-04
+ * Time: 22:24
+ */
+public class SparseArray {
+    public static void main(String[] args) {
+        //创建一个原始的二维数组11*11
+        //0:表示没有棋子，1表示黑子，2
+        int
+    }
+
+}
